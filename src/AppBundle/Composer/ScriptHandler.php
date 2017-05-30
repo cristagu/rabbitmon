fbb44636ce641b33989e88c57eda36ffe13c9e40
Cristian Neagu <cristian.neagu@emag.ro>
2017-05-30 16:19:41 +0300

    Added autodeployment of dbfile after composer install

--- src/AppBundle/Composer/ScriptHandler.php
+++ src/AppBundle/Composer/ScriptHandler.php
@@ -8,8 +8,41 @@
 
 namespace AppBundle\Composer;
 
+use Composer\Script\Event;
 
-class ScriptHandler
+class ScriptHandler extends \Sensio\Bundle\DistributionBundle\Composer\ScriptHandler
 {
+    /**
+     * Creates the sqlite database file
+     */
+    public static function databaseCreate(Event $event)
+    {
+        umask(0000);
 
+        $options = self::getOptions($event);
+        $consoleDir = self::getConsoleDir($event, 'database create');
+
+        if (null === $consoleDir) {
+            return;
+        }
+
+        static::executeCommand($event, $consoleDir, 'doctrine:database:create', $options['process-timeout']);
+    }
+
+    /**
+     * Call the demo command of the Acme Demo Bundle.
+     */
+    public static function schemaUpdate(Event $event)
+    {
+        umask(0000);
+
+        $options = self::getOptions($event);
+        $consoleDir = self::getConsoleDir($event, 'database create');
+
+        if (null === $consoleDir) {
+            return;
+        }
+
+        static::executeCommand($event, $consoleDir, 'doctrine:schema:update --force', $options['process-timeout']);
+    }
 }
